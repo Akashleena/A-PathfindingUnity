ba98469ea40fbf44e41b804201ae9d11158acc86
Apple <apple@Apples-MacBook-Pro.local>
2022-06-08 11:49:27 +0530

    Read data from json and save data to json files using button clicks on the game scene

---
+++ Astar/Assets/Scripts/GameData.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class InputData 
+{
+    public Vector3 startPos;
+    public Vector3 endPos;
+}
+
+[System.Serializable]
+public class OutputData
+{
+    public List<Vector3> wayPoints;
+}
+
+public class GameData : MonoBehaviour
+{
+    public Vector3 startPos;
+    public Vector3 endPos;
+
+    public List<Vector3> wayPoints;
+
+    public void LoadInputData(InputData inputData)
+    {
+        this.startPos = inputData.startPos;
+        this.endPos = inputData.endPos;
+    }
+
+    public void LoadOutputData(OutputData outputData, List<Vector3> points)
+    {
+        outputData.wayPoints = this.wayPoints = points;
+    }
+}
---
+++ Astar/Assets/Scripts/GameDataManager.cs
@@ -0,0 +1,75 @@
+// Add System.IO to work with files!
+using System.IO;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameDataManager : MonoBehaviour
+{
+    //File Path for the InputData
+    string inputDataFilePath;
+
+    //File Path for the Output Data
+    string outputDataFilePath;
+
+    // Create a GameData field.
+    [SerializeField]
+    public InputData inputGameData;
+
+    [SerializeField]
+    public OutputData outputGameData;
+
+    public GameData gameData;
+
+    public List<Vector3> wayPoints;
+
+    void Awake()
+    {
+        // Update the path once the persistent path exists.
+       // saveFile = Application.persistentDataPath + "/gamedata.json";
+        inputDataFilePath = Application.dataPath + "/inputData.json";
+
+        outputDataFilePath = Application.dataPath + "/outputData.json";
+    }
+
+    public void CopyData()
+    {
+
+    }
+
+    public void readFile()
+    {
+        // Does the file exist?
+        if (File.Exists(inputDataFilePath))
+        {
+            // Read the entire file and save its contents.
+            string fileContents = File.ReadAllText(inputDataFilePath);
+
+            // Deserialize the JSON data 
+            //  into a pattern matching the GameData class.
+            inputGameData = JsonUtility.FromJson<InputData>(fileContents);
+
+           gameData.LoadInputData(inputGameData);
+        }
+
+        Debug.Log("File Read");
+    }
+
+    public void writeFile()
+    {
+        //To Create a Sample InputData File
+       // inputGameData.startPos = gameData.startPos;
+        //inputGameData.endPos = gameData.endPos;
+        //string jsonString = JsonUtility.ToJson(inputGameData);
+        //File.WriteAllText(inputDataFilePath, jsonString);
+
+        gameData.LoadOutputData(outputGameData, wayPoints);
+
+        string jsonString = JsonUtility.ToJson(outputGameData);
+
+        // Write JSON to file.
+        File.WriteAllText(outputDataFilePath, jsonString);
+
+        Debug.Log("File Saved");
+    }
+}
